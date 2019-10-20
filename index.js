7c6a1d135ef1d717bca2fd0efacc45fa8b55504f
Karlos Daniel Pires da Silva <karlosdanielsilva7654@gmail.com>
2019-10-20 18:42:15 -0300

    Add index.js and directory structure for Sequelize and MVC pattern Crate the index.js file. Write the basic code for Express.js server startup. Create the .sequelizerc file. Write the Sequelize config in .sequelizerc. Create the src/ directory. Create the app/ directory into src/ . Create the controllers/ directory into src/app/ . Create the routes/ directory into src/app/controllers/ . Create the index.js file into src/app/controllers/routes/ . Create the models/ directory into src/app/ . Create the database/ directory into src/database/ . Create the config/ directory into src/database/ . Create the database.js file into src/database/config/ . Write the database access configurations in database.js.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const routes = require("./src/app/controllers/routes");
+
+const app = express();
+
+const port = process.argv[2];
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server is running at localhost:${port}`);
+});
---
+++ src/database/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "karlos",
+  password: "123456789",
+  database: "sequelize-example",
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+};
